8d9b7d749dc8fc0fe6d46de73a67574974a79de8
GK <Gikoskos@users.noreply.github.com>
2015-07-22 22:23:04 +0300

    fixed some spaces

--- algsorts.c
+++ algsorts.c
@@ -2,9 +2,10 @@
 
 void printArray(int *arrayPtr,int wall)
 {
-	for(i = 0; i < ARRAY_SIZE; i++){
+	for (i = 0; i < ARRAY_SIZE; i++) {
 		printf("%d ", *arrayPtr++);
-		if(wall == i+1) printf("\b|");
+		if (wall == i+1)
+			printf("\b|");
 	}
 	printf("\n\n");
 }
@@ -12,7 +13,7 @@ void printArray(int *arrayPtr,int wall)
 void fillArray(int *arrayPtr)
 {
 	srand (time(NULL));
-	for(i = 0; i < ARRAY_SIZE; i++)
+	for (i = 0; i < ARRAY_SIZE; i++)
 		*arrayPtr++ = RANDOM_INT(30);
 }
 
@@ -20,13 +21,11 @@ void insertionSort(int array[])
 {
 	int curr, index, wall;
 	
-	for(wall = 1; wall < ARRAY_SIZE; wall++){
+	for (wall = 1; wall < ARRAY_SIZE; wall++) {
 		curr = array[wall];
 		index = wall - 1;
-		while(index >= 0 && curr < array[index]){
-			array[index+1] = array[index];
-			index--;
-		}
+		while (index >= 0 && curr < array[index]) 
+			array[index+1] = array[index--];
 		array[index + 1] = curr;
 		printArray(&array[0],wall);
 	}
@@ -36,17 +35,17 @@ void selectionSort(int array[])
 {
 	int min, index, wall, c;
 	
-	for(wall = 0; wall < ARRAY_SIZE-1; wall++){
+	for (wall = 0; wall < ARRAY_SIZE-1; wall++) {
 		min = array[wall];
 		index = wall + 1;
-		while(index < ARRAY_SIZE){
-			if(min > array[index]){
+		while (index < ARRAY_SIZE) {
+			if (min > array[index]) {
 				min = array[index];
 				c = index;
 			}
 			index++;
 		}
-		if(min != array[wall]){
+		if (min != array[wall]) {
 			array[c] = array[wall];
 			array[wall] = min;
 		}
